a0a575df5b71b2e084daca7fb0d447347c1e1d99
RodRic99 <128597511+RodRic99@users.noreply.github.com>
2024-12-24 11:07:21 +0900

    Add files via upload
    이게진짜

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from pydantic import BaseModel
 import pandas as pd
 import sqlite3
@@ -11,6 +11,9 @@ path = './'
 
 import os
 import requests
+import json
+from typing import Optional
+from datetime import datetime
 import xml.etree.ElementTree as ET
 import pandas as pd
 import sys
@@ -20,9 +23,9 @@ sys.path.append(path)
 
 
 # 더 필요한 라이브러리 추가 -------------
-import old_emergency1218_2138 as em
+import emergency1218_2138 as em
 
-naver_id, naver_key='ztoukfrsyz','by6OZFl0dfROReNDIRiBSCmIItYbu5uYfb8saL8I'
+naver_id, naver_key='basrjpnowz','IXJeSwizs1aDacp561uU7kQ7cE6xY0WBwkfiwrLT'
 
 app = FastAPI()
 
@@ -183,3 +186,202 @@ def update_item_text(emer: Emergency):
             "fee3": log_instance.fee3,
             "path3":log_instance.path3
             }
+            
+# admin log 12/24 12:24  
+from typing import Optional
+       
+class AdminLog (BaseModel):
+    datetime: str
+    input_text: str
+    input_summary: str
+    input_latitude: float
+    input_longitude: float
+    em_class: int
+    hospital1: Optional[str] = ""
+    addr1: Optional[str] = ""
+    tel1: Optional[str] = ""
+    eta1: Optional[str] = ""
+    dist1: Optional[float] = 0
+    fee1: Optional[float] = 0
+    hospital2: Optional[str] = ""
+    addr2: Optional[str] = ""
+    tel2: Optional[str] = ""
+    eta2: Optional[str] = ""
+    dist2: Optional[float] = 0
+    fee2: Optional[float] = 0
+    hospital3: Optional[str] = ""
+    addr3: Optional[str] = ""
+    tel3: Optional[str] = ""
+    eta3: Optional[str] = ""
+    dist3: Optional[float] = 0
+    fee3: Optional[float] = 0
+
+@app.get('/items')
+def getLogList():
+    
+    conn = sqlite3.connect('./db/em.db')
+
+    query = """
+        SELECT datetime, input_text, input_summary, input_latitude, input_longitude, 
+            em_class, hospital1, addr1, tel1, eta1, dist1, fee1, 
+            hospital2, addr2, tel2, eta2, dist2, fee2, 
+            hospital3, addr3, tel3, eta3, dist3, fee3 
+        FROM emdata
+    """
+    df = pd.read_sql(query, conn)
+    
+    conn.close()
+    
+    result = []
+    for _, row in df.iterrows():
+        row_dict = row.to_dict()
+        row_dict = {k: (None if pd.isna(v) else v) for k, v in row_dict.items()}
+        
+        log_instance = AdminLog(**row_dict)
+        result.append({ "datetime": log_instance.datetime,
+            "input_text": log_instance.input_text,
+            "input_summary": log_instance.input_summary,
+            "input_latitude": log_instance.input_latitude,
+            "input_longitude": log_instance.input_longitude,
+            "em_class": log_instance.em_class,
+            "hospital1": log_instance.hospital1,
+            "addr1": log_instance.addr1,
+            "tel1": log_instance.tel1,
+            "eta1": log_instance.eta1,
+            "dist1": log_instance.dist1,
+            "fee1": log_instance.fee1,
+            "hospital2": log_instance.hospital2,
+            "addr2": log_instance.addr2,
+            "tel2": log_instance.tel2,
+            "eta2": log_instance.eta2,
+            "dist2": log_instance.dist2,
+            "fee2": log_instance.fee2,
+            "hospital3": log_instance.hospital3,
+            "addr3": log_instance.addr3,
+            "tel3": log_instance.tel3,
+            "eta3": log_instance.eta3,
+            "dist3": log_instance.dist3,
+            "fee3": log_instance.fee3
+            })
+        
+    return result
+
+### 12/24 10:09 홍규님 코드
+from typing import Union
+
+from fastapi import FastAPI, File, UploadFile,Form
+from fastapi.responses import JSONResponse
+import shutil
+import os
+from pydantic import BaseModel
+import pandas as pd
+import sqlite3
+import logging
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+path = './'
+
+import os
+import requests
+import xml.etree.ElementTree as ET
+import pandas as pd
+import sys
+from fastapi import HTTPException
+
+class Emergency_audio(BaseModel):
+    lat: float
+    lon: float
+
+
+
+UPLOAD_DIR = "self_audio"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+@app.post("/upload-mp3/")
+async def upload_mp3(emer: str = Form(...), file: UploadFile = File(...)):
+    try:
+        # emer를 위도와 경도로 분리
+        lat, lon = map(float, emer.split(","))
+        logger.info("lat&lon")
+        logger.info(lat)
+        logger.info(lon)
+    except ValueError:
+        logger.error(f"Invalid 'emer' format: {emer}")
+        raise HTTPException(status_code=400, detail="Invalid 'emer' format. Use 'latitude,longitude' format.")
+    
+    if not file.filename.endswith(".mp3"):
+        logger.error(f"Invalid file type: {file.filename}")
+        return JSONResponse({"error": "Invalid file type. Only MP3 files are allowed."}, status_code=400)
+
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
+    
+    try:
+        # Save the file locally
+        with open(file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+        logger.info(f"File {file.filename} saved successfully at {file_path}")
+    except Exception as e:
+        logger.error(f"Error while saving the file: {str(e)}")
+        raise HTTPException(status_code=500, detail="Error saving the file.")
+
+    try:
+        hospital_recommender = em.RecommendHospital3(
+            naver_id=naver_id,
+            naver_key=naver_key,
+            path=path,
+            filename=file.filename,
+            latitude=lat,
+            longitude=lon
+        )
+        hospital_recommender.load_api_key()
+        result_t = hospital_recommender.text_summary()
+        logger.info(result_t)
+        transcript, result_r, ilat, ilon, result_t = hospital_recommender.classify_situation()
+        result = hospital_recommender.search_map()
+    except Exception as e:
+        logger.error(f"Error during hospital recommender process: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Error: {str(e)}")
+    
+    if isinstance(result, str):
+        logger.info(f"Result: {result}")
+        return {"result": result}
+
+    if not result.empty:
+        row_data = result.iloc[0].to_dict()
+        log_instance = Log(**row_data)
+    else:
+        logger.warning("No hospital found.")
+        raise HTTPException(status_code=404, detail="No hospital found.")
+    
+    return {
+        "datetime": log_instance.datetime,
+        "input_text": log_instance.input_text,
+        "input_summary": log_instance.input_summary,
+        "input_latitude": log_instance.input_latitude,
+        "input_longitude": log_instance.input_longitude,
+        "em_class": log_instance.em_class,
+        "hospital1": log_instance.hospital1,
+        "addr1": log_instance.addr1,
+        "tel1": log_instance.tel1,
+        "eta1": log_instance.eta1,
+        "dist1": log_instance.dist1,
+        "fee1": log_instance.fee1,
+        "path1": log_instance.path1,
+        "hospital2": log_instance.hospital2,
+        "addr2": log_instance.addr2,
+        "tel2": log_instance.tel2,
+        "eta2": log_instance.eta2,
+        "dist2": log_instance.dist2,
+        "fee2": log_instance.fee2,
+        "path2": log_instance.path2,
+        "hospital3": log_instance.hospital3,
+        "addr3": log_instance.addr3,
+        "tel3": log_instance.tel3,
+        "eta3": log_instance.eta3,
+        "dist3": log_instance.dist3,
+        "fee3": log_instance.fee3,
+        "path3": log_instance.path3
+    }
